31f4ba623ecf31bd3911da5e86f096d869271770
Lana Chiad <lanachiad7@gmail.com>
2017-11-29 20:54:11 -0500

    Create new item with button with custom or default title

--- main.js
+++ main.js
@@ -26,4 +26,23 @@ $(document).ready(function() {
   //    If the user clicks "Add New" and the input is empty,
   //    the new item's title needs to default to Title {number} (ex: Title 6 or Title 7)
   //    depending on the number of items already on the page
+
+  $('.add').on('click', function(e) {
+    e.preventDefault();
+    const itemList = $('.item-list');
+    const newTitle = $('#newtitle').val();
+    const i = itemList.children().length;
+    let title;
+    if (newTitle.length > 0) {
+      title = newTitle;
+    } else {
+      title = 'Title ' + (i + 1);
+    }
+
+    itemList.append(
+      "<div class='item'><div class='head'><h1 class='title'>" +
+        title +
+        "</h1><span class='close'>x</span></div><p class='description'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris id scelerisque lacus, rutrum lobortis nisl. Ut dapibus, quam in auctor pharetra, nibh erat pharetra velit, vitae tristique mauris dui quis.</p></div>"
+    );
+  });
 });
